e0fcf8e105ae82dabc2e724a2e98994f53280008
guillaume Duplessis <guillaume.duplessis@thunder-stock.com>
2024-02-20 10:57:09 +0100

    Add Apollo Client and Graph Protocol client-cli dependencies

--- pages/profile/[profile].jsx
+++ pages/profile/[profile].jsx
@@ -2,8 +2,29 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import UserId from "../../components/userId";
 import Meta from "../../components/Meta";
+import { execute } from "../../.graphclient";
+import { gql } from "@apollo/client"; 
 
 const Edit_user = () => {
+  const GET_DATA = gql`
+    query MyQuery {
+      newDSponsorNFTs {
+        contractAddr
+        owner
+      }
+      updateOffers {
+        offerId
+        nftContract
+      }
+    }
+  `;
+  async function main() {
+    const resultat = await execute(GET_DATA, {});
+    console.log(resultat);
+  }
+
+  main();
+
   const [profilePhoto, setProfilePhoto] = useState();
   const [coverePhoto, setCoverePhoto] = useState();
   const [preview, setPreview] = useState();
@@ -57,35 +78,15 @@ const Edit_user = () => {
       <div className="pt-[5.5rem] lg:pt-24">
         {/* <!-- Banner --> */}
         <div className="relative">
-          <Image
-            width={1519}
-            height={300}
-            priority
-            src={coverPreview ? coverPreview : "/images/user/banner.jpg"}
-            alt="banner"
-            className="h-[18.75rem] w-full object-cover"
-          />
+          <Image width={1519} height={300} priority src={coverPreview ? coverPreview : "/images/user/banner.jpg"} alt="banner" className="h-[18.75rem] w-full object-cover" />
           <div className="container relative -translate-y-4">
             <div className="font-display group hover:bg-accent absolute right-0 bottom-4 flex items-center rounded-lg bg-white py-2 px-4 text-sm">
-              <input
-                type="file"
-                accept="image/*"
-                className="absolute inset-0 cursor-pointer opacity-0"
-                onChange={(e) => handleCoverPhoto(e)}
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-                className="fill-jacarta-400 mr-1 h-4 w-4 group-hover:fill-white"
-              >
+              <input type="file" accept="image/*" className="absolute inset-0 cursor-pointer opacity-0" onChange={(e) => handleCoverPhoto(e)} />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="fill-jacarta-400 mr-1 h-4 w-4 group-hover:fill-white">
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path d="M15.728 9.686l-1.414-1.414L5 17.586V19h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM7.242 21H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 21z"></path>
               </svg>
-              <span className="text-black mt-0.5 block group-hover:text-white">
-                Edit cover photo
-              </span>
+              <span className="text-black mt-0.5 block group-hover:text-white">Edit cover photo</span>
             </div>
           </div>
         </div>
@@ -93,14 +94,7 @@ const Edit_user = () => {
         {/* <!-- Edit Profile --> */}
         <section className="dark:bg-jacarta-800 relative py-16">
           <picture className="pointer-events-none absolute inset-0 -z-10 dark:hidden">
-            <Image
-              width={1519}
-              height={773}
-              priority
-              src="/images/gradient_light.jpg"
-              alt="gradient"
-              className="h-full w-full object-cover"
-            />
+            <Image width={1519} height={773} priority src="/images/gradient_light.jpg" alt="gradient" className="h-full w-full object-cover" />
           </picture>
           <div className="container">
             <div className="mx-auto max-w-[48.125rem] md:flex">
@@ -205,45 +199,24 @@ const Edit_user = () => {
                   </div>
                 </div>
                 <div className="mb-6">
-                  <label className="font-display text-jacarta-700 mb-1 block text-sm dark:text-white">
-                    Wallet Address
-                  </label>
+                  <label className="font-display text-jacarta-700 mb-1 block text-sm dark:text-white">Wallet Address</label>
 
                   <UserId
                     classes="js-copy-clipboard dark:bg-jacarta-700 border-jacarta-100 hover:bg-jacarta-50 dark:border-jacarta-600 dark:text-jacarta-300 flex w-full select-none items-center rounded-lg border bg-white py-3 px-4"
                     userId="0x7a9fe22691c811ea339401bbb2leb"
                   />
                 </div>
-                <button className="bg-accent shadow-accent-volume hover:bg-accent-dark rounded-full py-3 px-8 text-center font-semibold text-white transition-all">
-                  Update Profile
-                </button>
+                <button className="bg-accent shadow-accent-volume hover:bg-accent-dark rounded-full py-3 px-8 text-center font-semibold text-white transition-all">Update Profile</button>
               </div>
               {/* <!-- Avatar --> */}
               <div className="flex space-x-5 md:w-1/2 md:pl-8">
                 <form className="shrink-0">
                   <figure className="relative inline-block">
-                    <Image
-                      src={preview ? preview : "/images/user/user_avatar.gif"}
-                      alt="collection avatar"
-                      className="dark:border-jacarta-600 rounded-xl border-[5px] border-white"
-                      height={140}
-                      width={140}
-                    />
+                    <Image src={preview ? preview : "/images/user/user_avatar.gif"} alt="collection avatar" className="dark:border-jacarta-600 rounded-xl border-[5px] border-white" height={140} width={140} />
                     <div className="group hover:bg-accent border-jacarta-100 absolute -right-3 -bottom-2 h-8 w-8 overflow-hidden rounded-full border bg-white text-center hover:border-transparent">
-                      <input
-                        type="file"
-                        accept="image/*"
-                        className="absolute top-0 left-0 w-full cursor-pointer opacity-0"
-                        onChange={(e) => handleProfilePhoto(e)}
-                      />
+                      <input type="file" accept="image/*" className="absolute top-0 left-0 w-full cursor-pointer opacity-0" onChange={(e) => handleProfilePhoto(e)} />
                       <div className="flex h-full items-center justify-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          width="24"
-                          height="24"
-                          className="fill-jacarta-400 h-4 w-4 group-hover:fill-white"
-                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="fill-jacarta-400 h-4 w-4 group-hover:fill-white">
                           <path fill="none" d="M0 0h24v24H0z" />
                           <path d="M15.728 9.686l-1.414-1.414L5 17.586V19h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM7.242 21H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 21z" />
                         </svg>
@@ -252,13 +225,8 @@ const Edit_user = () => {
                   </figure>
                 </form>
                 <div className="mt-4">
-                  <span className="font-display text-jacarta-700 mb-3 block text-sm dark:text-white">
-                    Profile Image
-                  </span>
-                  <p className="dark:text-jacarta-300 text-sm leading-normal">
-                    We recommend an image of at least 300x300. Gifs work too.
-                    Max 5mb.
-                  </p>
+                  <span className="font-display text-jacarta-700 mb-3 block text-sm dark:text-white">Profile Image</span>
+                  <p className="dark:text-jacarta-300 text-sm leading-normal">We recommend an image of at least 300x300. Gifs work too. Max 5mb.</p>
                 </div>
               </div>
             </div>
