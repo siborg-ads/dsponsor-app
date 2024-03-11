fdcd49f0a21fda9ffed2ebd4660fb5ed621ae32c
guillaume Duplessis <guillaume.duplessis@thunder-stock.com>
2024-03-11 12:01:10 +0100

    Fix hover background color and add disabled button style

--- pages/create/index.jsx
+++ pages/create/index.jsx
@@ -14,11 +14,9 @@ const Create = () => {
   const fileTypes = ["JPG", "PNG", "SVG"];
   const [file, setFile] = useState(null);
   const { mutateAsync: upload, isLoading } = useStorageUpload();
-  const [uploadUrl, setUploadUrl] = useState(null);
   const [name, setName] = useState(null);
   const [link, setLink] = useState(null);
   const [errors, setErrors] = useState({});
-  const [ipfsLink, setIpfsLink] = useState(null);
   const [description, setDescription] = useState(null);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -28,10 +26,9 @@ const Create = () => {
   const [selectedCurrency, setSelectedCurrency] = useState("JEUR");
   const [customContract, setCustomContract] = useState(null);
   const [selectedRoyalties, setSelectedRoyalties] = useState(10);
-  const [jsonIpfsLink, setJsonIpfsLink] = useState(null);
   const [validate, setValidate] = useState(false);
-  const [args, setArgs] = useState([]);
   const [showPreviewModal, setShowPreviewModal] = useState(false);
+  const [successFullUpload, setSuccessFullUpload] = useState(false);
 
   const handleNumberChange = (e) => {
     setSelectedNumber(parseInt(e.target.value, 10));
@@ -57,7 +54,6 @@ const Create = () => {
   };
 
   const address = useAddress();
-  const switchChain = useSwitchChain();
   const { contract } = useContract("0xA82B4bBc8e6aC3C100bBc769F4aE0360E9ac9FC3"); // dsponsor admin mumbai contract address
 
   const { mutateAsync, isLoading: isLoadingContractWrite, error } = useContractWrite(contract, "createDSponsorNFTAndOffer");
@@ -81,7 +77,7 @@ const Create = () => {
 
   const validateInputs = () => {
     let isValid = true;
-    let newErrors = {}; // Objet pour stocker les nouvelles erreurs
+    let newErrors = {};
 
     if (!file) {
       newErrors.imageError = "Image is missing.";
@@ -139,14 +135,11 @@ const Create = () => {
       newErrors.royaltyError = "Royalties are missing or invalid. They should be between 0% and 100%.";
       isValid = false;
     }
-setValidate(isValid);
+    setValidate(isValid);
     setErrors(newErrors);
     return isValid;
   };
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(jsonIpfsLink);
-  };
   const handlePreviewModal = () => {
     setShowPreviewModal(!showPreviewModal);
     validateInputs();
@@ -162,9 +155,8 @@ setValidate(isValid);
       options: { uploadWithGatewayUrl: true, uploadWithoutDirectory: true },
     });
 
-    setUploadUrl(uploadUrl);
-    if (uploadUrl && name && link) {
-      onUpload(uploadUrl, name, link);
+    if (name && link) {
+      onUpload(name, link);
     } else {
       console.error("Missing name or link");
     }
@@ -187,30 +179,47 @@ setValidate(isValid);
       options: { uploadWithGatewayUrl: true, uploadWithoutDirectory: true },
     });
 
-    setJsonIpfsLink(jsonUrl[0]);
-    const preparedArgs = {
-      // Supposons que `args` est préparé correctement et contient toutes les données nécessaires
-      args: [Object.values(JSON.parse(args[0])), Object.values(JSON.parse(args[1]))],
-    };
+    const jsonIpfsLink = jsonUrl[0];
+
+    const args = [
+      JSON.stringify({
+        name: name, // name
+        symbol: "DSPONSORNFT", // symbol
+        baseURI: "https://api.dsponsor.com/tokenMetadata/", // baseURI
+        contractURI: jsonIpfsLink, // contractURI from json
+        minter: address,
+        maxSupply: selectedNumber, // max supply
+        forwarder: "0x0000000000000000000000000000000000000000", // forwarder
+        initialOwner: address, // owner
+        royaltyBps: selectedRoyalties * 100, // royalties
+        currencies: [selectedCurrencyContract(selectedCurrency)], // accepted token
+        prices: [BigNumber.from(selectedUnitPrice).mul(BigNumber.from(10).pow(getDecimals(selectedCurrency)))], // prices with decimals
+        allowedTokenIds: Array.from({ length: selectedNumber }, (_, i) => i), // allowed token ids
+      }),
+      JSON.stringify({
+        name: name, // name
+        rulesURI: jsonIpfsLink, // rulesURI
+        options: {
+          admins: [address], // admin
+          validators: [], // validator
+          adParameters: ["squareLogoURL", "linkURL"], // ad parameters
+        },
+      }),
+    ];
+    const preparedArgs = [Object.values(JSON.parse(args[0])), Object.values(JSON.parse(args[1]))];
+
     try {
-      const result = await mutateAsync(preparedArgs);
-      console.log("Mutation successful", result);
-
-      // Gérer le succès, par exemple en naviguant vers une nouvelle page ou en affichant un message de succès
+      await mutateAsync({ args: preparedArgs });
+      setSuccessFullUpload(true);
     } catch (error) {
-      console.error("Failed to write contract", error);
-      // Gérer l'erreur, par exemple en affichant un message d'erreur
+      setSuccessFullUpload(false);
     }
-
   };
 
-  const onUpload = (logo, updatedName, updatedLink) => {
-    setIpfsLink(logo);
+  const onUpload = (updatedName, updatedLink) => {
     setName(updatedName);
     setLink(updatedLink);
   };
-
-
 
   const { contract: JEURTokenContract } = useContract("0xd409F17095a370800A9C352124C6a1e82695203E", "token"); // mumbai
 
@@ -264,45 +273,6 @@ setValidate(isValid);
     },
     [JEURdecimals, USDCDecimals, WETHDecimals, MaticDecimals, customDecimals]
   );
-
-  const updateArgs = useCallback(() => {
-    if (!selectedCurrency) return;
-    if (!selectedUnitPrice) return;
-    if (!selectedNumber) return;
-    if (!selectedRoyalties) return;
-    if (!name) return;
-    if (!jsonIpfsLink) return;
-
-    setArgs([
-      JSON.stringify({
-        name: name, // name
-        symbol: "DSPONSORNFT", // symbol
-        baseURI: "https://api.dsponsor.com/tokenMetadata/", // baseURI
-        contractURI: jsonIpfsLink, // contractURI from json
-        minter: address,
-        maxSupply: selectedNumber, // max supply
-        forwarder: "0x0000000000000000000000000000000000000000", // forwarder
-        initialOwner: address, // owner
-        royaltyBps: selectedRoyalties * 100, // royalties
-        currencies: [selectedCurrencyContract(selectedCurrency)], // accepted token
-        prices: [BigNumber.from(selectedUnitPrice).mul(BigNumber.from(10).pow(getDecimals(selectedCurrency)))], // prices with decimals
-        allowedTokenIds: Array.from({ length: selectedNumber }, (_, i) => i), // allowed token ids
-      }),
-      JSON.stringify({
-        name: name, // name
-        rulesURI: jsonIpfsLink, // rulesURI
-        options: {
-          admins: [address], // admin
-          validators: [], // validator
-          adParameters: ["squareLogoURL", "linkURL"], // ad parameters
-        },
-      }),
-    ]);
-  }, [address, getDecimals, jsonIpfsLink, name, selectedCurrency, selectedCurrencyContract, selectedNumber, selectedRoyalties, selectedUnitPrice]);
-
-  useEffect(() => {
-    updateArgs();
-  }, [updateArgs]);
 
   const [currentSlide, setCurrentSlide] = useState(0);
   const stepContainerRef = useRef(null);
@@ -393,7 +363,7 @@ setValidate(isValid);
                 <div className={styles.form__step__container} ref={stepContainerRef}>
                   {/* Step 1 */}
                   <div ref={(el) => (stepsRef.current[0] = el)} className={styles.form__step}>
-                    <div className="pr-6">
+                    <div className="pr-6 pl-2">
                       <h3 className="mb-14">Step 1</h3>
                       {/* <!-- Name --> */}
                       <div className="mb-6">
@@ -427,7 +397,7 @@ setValidate(isValid);
                   </div>
                   {/* Step 2 */}
                   <div ref={(el) => (stepsRef.current[1] = el)} className={styles.form__step}>
-                    <div className="pr-6">
+                    <div className="pr-6 pl-2">
                       <h3 className="mb-14">Step 2</h3>
 
                       {/* <!-- External Link --> */}
@@ -473,7 +443,7 @@ setValidate(isValid);
                   </div>
                   {/* Step 3 */}
                   <div ref={(el) => (stepsRef.current[2] = el)} className={styles.form__step}>
-                    <div className="pr-6">
+                    <div className="pr-6 pl-2">
                       <h3 className="mb-14">Step 3</h3>
                       {/* <!-- Validity period --> */}
                       <div className="mb-6 flex flex-col items-center">
@@ -528,7 +498,7 @@ setValidate(isValid);
                   {/* Step 4 */}
                   <div ref={(el) => (stepsRef.current[3] = el)} className={styles.form__step}>
                     {/* <!-- Unit selling price --> */}
-                    <div className="pr-6">
+                    <div className="pr-6 pl-2">
                       <h3 className="mb-14">Step 4</h3>
                       <div className="mb-6 flex flex-col items-center">
                         <label htmlFor="item-description" className="font-display text-jacarta-700 mb-2 block dark:text-white">
@@ -632,57 +602,6 @@ setValidate(isValid);
             </div>
           </div>
         </div>
-        {/* <!-- Submit --> */}
-        <div className="flex items-center gap-4">
-          {!validate && (
-            <button className="bg-accent cursor-pointer rounded-full py-3 px-8 text-center font-semibold text-white transition-all" onClick={handleSubmit}>
-              Validate
-            </button>
-          )}
-          {validate && (
-            <button className="bg-accent cursor-pointer rounded-full py-3 px-8 text-center font-semibold text-white transition-all" onClick={handleSubmit}>
-              Revalidate
-            </button>
-          )}
-          {validate && (
-            <Web3Button
-              contractAddress="0xA82B4bBc8e6aC3C100bBc769F4aE0360E9ac9FC3"
-              action={() =>
-                mutateAsync({
-                  args: [Object.values(JSON.parse(args[0])), Object.values(JSON.parse(args[1]))],
-                })
-              }
-              className="!bg-accent !cursor-pointer !rounded-full !py-3 !px-8 !text-center !font-semibold !text-white !transition-all"
-            >
-              Create Ad Space Offer
-            </Web3Button>
-          )}
-        </div>
-        {!jsonIpfsLink && <></>}
-        <div className="flex justify-start gap-2 mt-4">
-          {file && !jsonIpfsLink && isLoading && (
-            <div className="flex items-center">
-              <div className="inline-block mr-2 h-4 w-4 animate-spin rounded-full border border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
-              Uploading...
-            </div>
-          )}
-          {jsonIpfsLink && (
-            <div className={`${jsonIpfsLink && "flex flex-col gap-4"}`}>
-              <div className="flex items-center gap-2">
-                <p className="font-light">The item has been uploaded to IPFS.</p>
-                <button onClick={copyToClipboard}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-500 active:text-black">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
       </section>
       {showPreviewModal && (
         <div className="modal fade show bloc">
@@ -703,6 +622,8 @@ setValidate(isValid);
             previewImage={previewImage}
             validate={validate}
             errors={errors}
+            successFullUpload={successFullUpload}
+            address={address}
           />
         </div>
       )}
