7ea46cd41dbb75b6552cd38e53d23823509dfb20
Rim <souabni.rim@gmail.com>
2024-04-02 14:12:13 +0200

    init app route first page - marketplace

---
+++ app/marketplace/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Marketplace(){
+  return (
+    <section>
+      <h1 className="text-2xl text-black">
+        Hey
+      </h1>
+    </section>
+  );
+}
--- components/header/Header01.jsx
+++ components/header/Header01.jsx
@@ -4,7 +4,10 @@ import DarkMode from "../mode/DarkMode";
 import Logo from "./../../public/images/logo.png";
 import WhiteLogo from "./../../public/images/logo_white.png";
 import { useRouter } from "next/router";
-import { isChildrenPageActive, isParentPageActive } from "../../utils/daynamicNavigation";
+import {
+  isChildrenPageActive,
+  isParentPageActive,
+} from "../../utils/daynamicNavigation";
 import { useEffect, useState } from "react";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import { useTheme } from "next-themes";
@@ -43,11 +46,22 @@ export default function Header01() {
         <div className="flex items-center px-6 py-6 xl:px-24 ">
           <Link className="shrink-0" href="/">
             <div className="dark:hidden flex justify-center items-center  font-semibold">
-              <Image src={Logo} height={80} width={80} alt="DSponsor | smarter monetization for your content" className=" h-auto " />
+              <Image
+                src={Logo}
+                height={80}
+                width={80}
+                alt="DSponsor | smarter monetization for your content"
+                className=" h-auto "
+              />
               <span>d&gt;sponsor</span>
             </div>
             <div className="hidden dark:flex flex justify-center items-center text-white font-semibold">
-              <Image src={WhiteLogo} height={80} width={80} alt="DSponsor | smarter monetization for your content" />
+              <Image
+                src={WhiteLogo}
+                height={80}
+                width={80}
+                alt="DSponsor | smarter monetization for your content"
+              />
               <span>d&gt;sponsor</span>
             </div>
           </Link>
@@ -60,7 +74,15 @@ export default function Header01() {
                 <li className="group">
                   <Link href="/#hot-offers">
                     <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                      <span className={isChildrenPageActive(route.asPath, "#hot-offers") ? "text-accent dark:text-accent" : ""}>Buy spaces</span>
+                      <span
+                        className={
+                          isChildrenPageActive(route.asPath, "#hot-offers")
+                            ? "text-accent dark:text-accent"
+                            : ""
+                        }
+                      >
+                        Buy spaces
+                      </span>
                     </button>
                   </Link>
                 </li>
@@ -68,7 +90,31 @@ export default function Header01() {
                 <li className="group">
                   <Link href="/offer/create">
                     <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                      <span className={isChildrenPageActive(route.asPath, "/offer/create") ? "text-accent dark:text-accent" : ""}>Create offer</span>
+                      <span
+                        className={
+                          isChildrenPageActive(route.asPath, "/offer/create")
+                            ? "text-accent dark:text-accent"
+                            : ""
+                        }
+                      >
+                        Create offer
+                      </span>
+                    </button>
+                  </Link>
+                </li>
+                {/* marketplace */}
+                <li className="group">
+                  <Link href="/marketplace">
+                    <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
+                      <span
+                        className={
+                          isChildrenPageActive(route.asPath, "/marketplace")
+                            ? "text-accent dark:text-accent"
+                            : ""
+                        }
+                      >
+                        Marketplace
+                      </span>
                     </button>
                   </Link>
                 </li>
@@ -76,12 +122,27 @@ export default function Header01() {
                   <li className="group">
                     <Link href={`/manageSpaces/${address}`}>
                       <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                        <span className={isChildrenPageActive(route.asPath, `/user/${address}`) ? "text-accent dark:text-accent" : ""}>Manage</span>
+                        <span
+                          className={
+                            isChildrenPageActive(
+                              route.asPath,
+                              `/user/${address}`
+                            )
+                              ? "text-accent dark:text-accent"
+                              : ""
+                          }
+                        >
+                          Manage
+                        </span>
                       </button>
                     </Link>
                   </li>
                 )}
                 <li className="group">
+                  <ConnectWallet
+                    theme={theme ? "light" : "dark"}
+                    modalSize={"wide"}
+                  />
                   <ConnectWallet theme={"dark"} modalSize={"wide"} />
                 </li>
               </ul>
@@ -98,7 +159,13 @@ export default function Header01() {
               aria-label="open mobile menu"
               onClick={() => setToggle(true)}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width={24}
+                height={24}
+                className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
+              >
                 <path fill="none" d="M0 0h24v24H0z" />
                 <path d="M18 18v2H6v-2h12zm3-7v2H3v-2h18zm-3-7v2H6V4h12z" />
               </svg>
@@ -118,18 +185,35 @@ export default function Header01() {
       >
         <div className="t-0 dark:bg-jacarta-800 left-0 z-10 flex w-full items-center justify-between bg-white p-6 lg:hidden">
           <div className="dark:hidden">
-            <Image src={Logo} height={80} width={80} alt="DSponsor | smarter monetization for your content" className="max-h-7 h-auto " />
+            <Image
+              src={Logo}
+              height={80}
+              width={80}
+              alt="DSponsor | smarter monetization for your content"
+              className="max-h-7 h-auto "
+            />
           </div>
 
           <div className="hidden dark:block">
-            <Image src={WhiteLogo} height={80} width={80} alt="DSponsor | smarter monetization for your content" />
+            <Image
+              src={WhiteLogo}
+              height={80}
+              width={80}
+              alt="DSponsor | smarter monetization for your content"
+            />
           </div>
 
           <button
             className="js-mobile-close border-jacarta-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]"
             onClick={() => setToggle(false)}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width={24}
+              height={24}
+              className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
+            >
               <path fill="none" d="M0 0h24v24H0z" />
               <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
             </svg>
@@ -142,14 +226,30 @@ export default function Header01() {
             <li className="group">
               <Link href="/" onClick={() => setToggle(false)}>
                 <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                  <span className={isChildrenPageActive(route.asPath, "/") ? "text-accent dark:text-accent" : ""}>Home</span>
+                  <span
+                    className={
+                      isChildrenPageActive(route.asPath, "/")
+                        ? "text-accent dark:text-accent"
+                        : ""
+                    }
+                  >
+                    Home
+                  </span>
                 </button>
               </Link>
             </li>
             <li className="group">
               <Link href="/#hot-offers" onClick={() => setToggle(false)}>
                 <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                  <span className={isChildrenPageActive(route.asPath, "#hot-offers") ? "text-accent dark:text-accent" : ""}>Buy spaces</span>
+                  <span
+                    className={
+                      isChildrenPageActive(route.asPath, "#hot-offers")
+                        ? "text-accent dark:text-accent"
+                        : ""
+                    }
+                  >
+                    Buy spaces
+                  </span>
                 </button>
               </Link>
             </li>
@@ -157,7 +257,15 @@ export default function Header01() {
             <li className="group">
               <Link href="/offer/create" onClick={() => setToggle(false)}>
                 <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                  <span className={isChildrenPageActive(route.asPath, "/offer/create") ? "text-accent dark:text-accent" : ""}>Create offer</span>
+                  <span
+                    className={
+                      isChildrenPageActive(route.asPath, "/offer/create")
+                        ? "text-accent dark:text-accent"
+                        : ""
+                    }
+                  >
+                    Create offer
+                  </span>
                 </button>
               </Link>
             </li>
@@ -165,7 +273,15 @@ export default function Header01() {
               <li className="group" onClick={() => setToggle(false)}>
                 <Link href={`/manageSpaces/${address}`}>
                   <button className="text-jacarta-700 font-display hover:text-accent focus:text-accent dark:hover:text-accent dark:focus:text-accent flex items-center justify-between py-3.5 text-base dark:text-white lg:px-5">
-                    <span className={isChildrenPageActive(route.asPath, `/user/${address}`) ? "text-accent dark:text-accent" : ""}>Manage</span>
+                    <span
+                      className={
+                        isChildrenPageActive(route.asPath, `/user/${address}`)
+                          ? "text-accent dark:text-accent"
+                          : ""
+                      }
+                    >
+                      Manage
+                    </span>
                   </button>
                 </Link>
               </li>
