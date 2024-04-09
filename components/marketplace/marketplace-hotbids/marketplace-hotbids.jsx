51dd0d5fe19052ecf10f174e12b69a7e5ab98d47
jubayerjuhan <davidjuhan23@gmail.com>
2024-04-10 02:48:19 +0600

    Added Marketplace Hotbids Section and Item Cards

--- components/marketplace/marketplace-hotbids/marketplace-hotbids.jsx
+++ components/marketplace/marketplace-hotbids/marketplace-hotbids.jsx
@@ -1,12 +1,26 @@
+"use client";
+
 import React from "react";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import MarketplaceItemCard from "../marketplace-item-card/marketplace-item-card";
 
 const MarketplaceHotbids = () => {
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [contractData, setContractData] = useState(null);
+
   return (
     <div className="container pt-28">
-      <h1 className="font-display tracking-wide	 text-center text-2xl font-medium text-jacarta-700 dark:text-white">
+      <h1 className="font-display tracking-wide mb-6	 text-center text-2xl font-medium text-jacarta-700 dark:text-white">
         Hot Bids
       </h1>
-      <div></div>
+      <div className={"grid grid-cols-4 gap-8"}>
+        <MarketplaceItemCard />
+        <MarketplaceItemCard />
+        <MarketplaceItemCard />
+        <MarketplaceItemCard />
+      </div>
     </div>
   );
 };
