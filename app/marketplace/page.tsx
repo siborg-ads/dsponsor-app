3396e0399d0fe0bd88587937c332aa1f01c4c2cb
jubayerjuhan <davidjuhan23@gmail.com>
2024-04-09 14:29:17 +0600

    Added Social Links Icons and Marketplace Hero Wrapped Under The Marketplace Page

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -2,5 +2,13 @@ import React from "react";
 import MarketplaceHeroSection from "../../components/marketplace-hero/marketplace-hero";
 
 export default function Marketplace() {
-  return <MarketplaceHeroSection />;
+  return (
+    <section
+      style={{
+        padding: "8rem 0",
+      }}
+    >
+      <MarketplaceHeroSection />
+    </section>
+  );
 }
