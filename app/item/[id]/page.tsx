be6fb0e022b67c6610427643217a874919dacc31
Rim <souabni.rim@gmail.com>
2024-04-11 12:34:52 +0200

    listing data fetched, buy/bid displayed depends on type, timer w/ endTime done

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -2,15 +2,11 @@
 
 import React from "react";
 import ItemDetails from "../../../components/item/ItemDetails";
-import Header01 from "../../../components/header/Header01";
 
 export default function Item({ params }){
   return (
     <>
-      {/* <Header01/> */}
-      <main>
-          <ItemDetails id={params.id}/>
-      </main>
+        <ItemDetails id={params.id}/>
     </>
   );
 }
