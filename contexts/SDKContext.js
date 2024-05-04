14b94066944c9c9320259dbd9c41ead5cc9b6f3f
Alex Werner <obusco@gmail.com>
2024-05-04 16:21:11 +0200

    feat(Provider): provide with local chain name and id

--- contexts/SDKContext.js
+++ contexts/SDKContext.js
@@ -4,7 +4,10 @@ import { createContext } from 'react';
 const SDKContext = createContext({
     sdk: null,
     admin: null,
+    SDKChainId: null,
+    setSDKChainId: () => {},
     setSDK: () => {},
+    getChainName: () => {},
     setAdmin: () => {},
 });
 export default SDKContext;
