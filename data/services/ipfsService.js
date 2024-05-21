5c644ed25614adf59511fa9e0282f1d5782ccab6
Alex Werner <obusco@gmail.com>
2024-05-21 09:45:40 +0200

    chore: remove axios

--- data/services/ipfsService.js
+++ data/services/ipfsService.js
@@ -1,15 +1,23 @@
-
-import axios from "axios";
-
 export const fetchDataFromIPFS = async (ipfsLink) => {
   try {
-   
-    new URL(ipfsLink); 
 
-    const response = await axios.get(ipfsLink);
-    return response.data; 
+    // new URL(ipfsLink);
+    const url = new URL(ipfsLink);
+    console.log(url, "URL");
+
+    const path = url.pathname;
+    console.log(path, "Path");
+
+    const ipfsGateway = "https://ipfs.io/ipfs/";
+    const ipfsUrl = `${ipfsGateway}${path}`;
+
+    console.log(ipfsUrl, "IPFS URL");
+
+    const response = await fetch(ipfsLink);
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error("Erreur lors de la récupération des données IPFS:", error);
-    return null; 
+    return null;
   }
 };
