795f01f58c6923576a0fdaa0f8fe51ffc7e0274e
Alex Werner <obusco@gmail.com>
2024-06-04 18:14:55 +0200

    feat: provide terms and conditions

---
+++ pages/terms-and-conditions/index.jsx
@@ -0,0 +1,5 @@
+import TermsAndConditionsContainer from "../../containers/TermsAndConditions/TermsAndConditionsContainer";
+
+export default function TermsAndConditionsPage() {
+    return <TermsAndConditionsContainer  />;
+}
