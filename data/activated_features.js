585bd48e9b618229e23f2a403ee647de62a046a1
Alex Werner <obusco@gmail.com>
2024-06-05 16:38:22 +0200

    feat: separate dev and prod environment available features

--- data/activated_features.js
+++ data/activated_features.js
@@ -1,7 +1,14 @@
 // This is not a secure way to handle feature flags.
 // If a new feature is added there, it shall be known that one user can just devtool change it
+
+const env = process.env.NODE_ENV
+const isProduction = env === 'production'
+const isDevelopment = env === 'development'
+
+// In development mode, we display create offer and multiple currencies (by default to allow easier dev).
+// We also later have those put in env variables to allow for easier testing
 export const activated_features = {
-    canCreateOffer: false,
+    canCreateOffer: !!isDevelopment,
     // by default, first item used
-    canHaveMultipleCurrencies: false,
+    canHaveMultipleCurrencies: !!isDevelopment,
 }
