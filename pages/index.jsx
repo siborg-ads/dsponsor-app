463dfb3008708cdbade0920c3fb47da31b994dac
Alexandre Trotel <trotelalexandre@proton.me>
2024-06-07 16:03:34 +0200

    fix: import container

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import HomePageContainer from "../containers/HomePage/HomePageContainer";
+import SiBorgAdsHomeContainer from "../containers/SiBorgAds/SiBorgAdsHomeContainer";
 
 export default function DefaultPage() {
   //return <HomePageContainer />;
