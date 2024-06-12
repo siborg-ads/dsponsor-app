3170451a65446db2451937cb35aab2752eea4b0f
Alex Werner <obusco@gmail.com>
2024-06-12 23:27:56 +0200

    feat: fixed middlewar + shorter time

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  console.log("Middleware request:", request);
   const url = new URL(request.nextUrl);
   const _rid = url.searchParams.get("_rid");
-  console.log("Middleware _rid:", _rid);
-  console.log("URL:", url.href);
 
   if (_rid) {
     const expires = new Date();
-    expires.setDate(expires.getDate() + 30);
+    const expireDays = 2;
+    expires.setDate(expires.getDate() + expireDays);
     const response = NextResponse.next();
     response.cookies.set("_rid", _rid, {
       expires
     });
+    console.log("Setting _rid cookie", _rid, "expires", expires);
     return response;
   }
 
@@ -21,8 +20,7 @@ export function middleware(request) {
 }
 
 export const config = {
-  // All pages
-  // Matcher ignoring `/_next/`, `/api/` '/images' and '/public' subpaths
+  // All pages but ignoring `/_next/`, `/api/` '/images' and '/public' subpaths
   matcher: [
     "/((?!_next/static|_next/image|robots.txt|public|images|manifest.json|sw.js|favicon.ico|workbox-*).*)",
     "/"
