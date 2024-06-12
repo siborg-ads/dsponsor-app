9d46097156de77efafc9b7be0216439e38dae593
Alex Werner <obusco@gmail.com>
2024-06-12 23:27:41 +0200

    chore: move all to src

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
+  console.log("Middleware request:", request);
   const url = new URL(request.nextUrl);
   const _rid = url.searchParams.get("_rid");
+  console.log("Middleware _rid:", _rid);
+  console.log("URL:", url.href);
 
   if (_rid) {
     const expires = new Date();
@@ -16,3 +19,12 @@ export function middleware(request) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  // All pages
+  // Matcher ignoring `/_next/`, `/api/` '/images' and '/public' subpaths
+  matcher: [
+    "/((?!_next/static|_next/image|robots.txt|public|images|manifest.json|sw.js|favicon.ico|workbox-*).*)",
+    "/"
+  ]
+};
