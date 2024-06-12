85acb27579bc3e589380282c2ddac3fedbe72d0d
Alex Werner <obusco@gmail.com>
2024-06-12 23:09:47 +0200

    chore: lint

--- containers/marketplace/MarketplaceLeaderboard/MarketplaceLeaderboardContainer.jsx
+++ containers/marketplace/MarketplaceLeaderboard/MarketplaceLeaderboardContainer.jsx
@@ -8,58 +8,56 @@ const MarketplaceLeaderboardContainer = () => {
   const [activity, setActivity] = useState([]);
   const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const chainIds = [11155111, 84532];
-        const promises = chainIds.map(chainId => fetchMarketplaceActivity(chainId).then(activity => ({
+  useEffect(() => {
+    const chainIds = [11155111, 84532];
+    const promises = chainIds.map((chainId) =>
+      fetchMarketplaceActivity(chainId).then((activity) => ({
+        ...activity,
+        chainId
+      }))
+    );
+
+    Promise.all(promises)
+      .then((activities) => {
+        const mergedActivitiesArray = [];
+        for (const activity of activities) {
+          const mergedActivities = {
             ...activity,
-            chainId
-        })));
-       
+            rankings: processBidsAndCalculateRewards(activity.rankings)
+          };
+          mergedActivitiesArray.push(mergedActivities);
+        }
+        console.log("Processed activity:", mergedActivitiesArray);
+        // const mergedActivities = activities.flatMap(activity => activity.rankings.map(ranking => ({
+        //     ...ranking,
+        //     chainId: activity.chainId
+        // })));
+        // console.log("Processed activity:", mergedActivities);
+        // const processedActivity = processBidsAndCalculateRewards(mergedActivities);
+        setActivity(mergedActivitiesArray);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching activities:", error);
+        setLoading(false);
+      });
+  }, []);
 
-        Promise.all(promises).then(activities => {
-           
-            const mergedActivitiesArray = [];
-            for(const activity of activities){
-            const mergedActivities = {
-              ...activity,
-              rankings: processBidsAndCalculateRewards(activity.rankings),
-            };
-            mergedActivitiesArray.push(mergedActivities);
-            }
-            console.log("Processed activity:", mergedActivitiesArray);
-            // const mergedActivities = activities.flatMap(activity => activity.rankings.map(ranking => ({
-            //     ...ranking,
-            //     chainId: activity.chainId
-            // })));
-            // console.log("Processed activity:", mergedActivities);
-            // const processedActivity = processBidsAndCalculateRewards(mergedActivities);
-            setActivity(mergedActivitiesArray);
-            setLoading(false);
-        }).catch((error) => {
-            console.error('Error fetching activities:', error);
-            setLoading(false);
-        });
-    }, []);
-
-    if (loading) {
-        return (
-          <>
-            <LeaderBoardSkeleton />
-          </>
-        ); 
-    }
-
+  if (loading) {
     return (
       <>
-        <section className="relative py-24">
-          
-            
-              <LeaderboardTable activity={activity} />
-            
-          
-        </section>
+        <LeaderBoardSkeleton />
       </>
     );
+  }
+
+  return (
+    <>
+      <section className="relative py-24">
+        <LeaderboardTable activity={activity} />
+      </section>
+    </>
+  );
 };
 
 export default MarketplaceLeaderboardContainer;
