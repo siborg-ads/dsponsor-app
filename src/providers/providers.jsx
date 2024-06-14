cdb25470caec3e8a9b6f03347a91e7b598df3e87
Alex Werner <obusco@gmail.com>
2024-06-14 16:14:20 +0200

    feat: provide base as supported chain

--- src/providers/providers.jsx
+++ src/providers/providers.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, createContext, useContext, useChain, use } from "react";
 import { useNetwork } from "@thirdweb-dev/react";
-import { Ethereum, Polygon, BaseSepoliaTestnet, Sepolia } from "@thirdweb-dev/chains";
+import { Ethereum, Polygon, BaseSepoliaTestnet, Sepolia, Base } from "@thirdweb-dev/chains";
 
 import {
   coinbaseWallet,
@@ -37,7 +37,7 @@ function InnerProviders({ children }) {
     <ThirdwebProvider
       activeChain={chain}
       clientId="6f375d41f2a33f1f08f6042a65d49ec9"
-      supportedChains={[Ethereum, Polygon, BaseSepoliaTestnet, Sepolia]}
+      supportedChains={[Ethereum, Polygon, BaseSepoliaTestnet, Sepolia, Base]}
       authConfig={{ domain: "dsponsor.com" }}
       supportedWallets={[
         metamaskWallet(),
