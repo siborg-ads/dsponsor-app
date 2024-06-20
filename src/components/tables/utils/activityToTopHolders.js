06e55f33122b48d806a42171de3e038c6f9fced9
Alexandre Trotel <trotelalexandre@proton.me>
2024-06-20 11:49:54 +0200

    fix: leaderboard

--- src/components/tables/utils/activityToTopHolders.js
+++ src/components/tables/utils/activityToTopHolders.js
@@ -1,12 +1,15 @@
+import { user } from "@nextui-org/react";
+import { getAddress } from "ethers/lib/utils";
+
 /**
  * Converts activity data to top holders format.
  * @param {Array} activity - The activity data.
+ * @param {string} userAddress - The user's address.
  * @returns {Array} Formatted top holders data.
  */
-const activityToTopHolders = (activity) => {
-  return activity
-    .sort((a, b) => a.holdersRank - b.holdersRank)
-    .map((ranking) => ({
+const activityToTopHolders = (activity, userAddress) => {
+  if (userAddress === undefined) {
+    return activity.map((ranking) => ({
       rank: ranking.holdersRank,
       totalSpent: ranking.usdcAmounts.totalSpent,
       addressDisplay: ranking.displayAddr,
@@ -19,6 +22,28 @@ const activityToTopHolders = (activity) => {
         totalSpent: amounts.totalSpent
       }))
     }));
+  }
+
+  const userActivity = activity.filter((item) => getAddress(item.addr) === getAddress(userAddress));
+  const otherActivity = activity;
+
+  const sortedOtherActivity = otherActivity.sort((a, b) => a.holdersRank - b.holdersRank);
+
+  const sortedActivity = [...userActivity, ...sortedOtherActivity];
+
+  return sortedActivity.map((ranking) => ({
+    rank: ranking.holdersRank,
+    totalSpent: ranking.usdcAmounts.totalSpent,
+    addressDisplay: getAddress(ranking.addr) === getAddress(userAddress) ? "You" : ranking.addr,
+    address: ranking.addr,
+    balance: ranking.balance,
+    chainId: ranking.chainId,
+    dPoints: ranking.dPoints ?? 0,
+    details: Object.entries(ranking.currenciesAmounts || {}).map(([currency, amounts]) => ({
+      currency,
+      totalSpent: amounts.totalSpent
+    }))
+  }));
 };
 
 export default activityToTopHolders;
--- src/components/tables/utils/activityToTopRewarded.js
+++ src/components/tables/utils/activityToTopRewarded.js
@@ -1,12 +1,14 @@
+import { getAddress } from "ethers/lib/utils";
+
 /**
  * Converts activity data to top rewarded format.
  * @param {Array} activity - The activity data.
+ * @param {string} userAddress - The user's address.
  * @returns {Array} Formatted top rewarded data.
  */
-const activityToTopRewarded = (activity) => {
-  return activity
-    .sort((a, b) => a.bidRefundsRank - b.bidRefundsRank)
-    .map((ranking) => ({
+const activityToTopRewarded = (activity, userAddress) => {
+  if (userAddress === undefined) {
+    return activity.map((ranking) => ({
       rank: ranking.bidRefundsRank,
       totalReceived: ranking.usdcAmounts.bidRefundReceived,
       addressDisplay: ranking.displayAddr,
@@ -19,6 +21,28 @@ const activityToTopRewarded = (activity) => {
         totalReceived: amounts.bidRefundReceived
       }))
     }));
+  }
+
+  const userActivity = activity.filter((item) => getAddress(item.addr) === getAddress(userAddress));
+  const otherActivity = activity;
+
+  const sortedOtherActivity = otherActivity.sort((a, b) => a.bidRefundsRank - b.bidRefundsRank);
+
+  const sortedActivity = [...userActivity, ...sortedOtherActivity];
+
+  return sortedActivity.map((ranking) => ({
+    rank: ranking.bidRefundsRank,
+    totalReceived: ranking.usdcAmounts.bidRefundReceived,
+    addressDisplay: getAddress(ranking.addr) === getAddress(userAddress) ? "You" : ranking.addr,
+    address: ranking.addr,
+    refunds: ranking.nbRefunds,
+    chainId: ranking.chainId,
+    dPoints: ranking.dPoints ?? 0,
+    details: Object.entries(ranking.currenciesAmounts || {}).map(([currency, amounts]) => ({
+      currency,
+      totalReceived: amounts.bidRefundReceived
+    }))
+  }));
 };
 
 export default activityToTopRewarded;
