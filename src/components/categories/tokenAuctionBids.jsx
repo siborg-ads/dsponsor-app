45b793cabe39ac6fa18f458b60df1c92715e8b4a
guillaume Duplessis <guillaumeduplessis@yahoo.fr>
2024-06-21 16:50:12 +0200

    fix: Update offerItem component to display sold status for token items

--- src/components/categories/tokenAuctionBids.jsx
+++ src/components/categories/tokenAuctionBids.jsx
@@ -4,6 +4,7 @@ import "tippy.js/themes/light.css";
 import OfferItem from "../cards/offerItem";
 
 const TokenAuctionBids = ({ data, isOwner }) => {
+  console.log(data);
   if (!data) {
     return (
       <div className="flex w-full justify-center">
