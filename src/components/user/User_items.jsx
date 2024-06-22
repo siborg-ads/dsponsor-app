5c642cb7382d9eaa0b71ea223c93f439f32cc730
Alexandre Trotel <trotelalexandre@proton.me>
2024-06-22 18:31:47 +0200

    fix: wording

--- src/components/user/User_items.jsx
+++ src/components/user/User_items.jsx
@@ -124,7 +124,7 @@ const User_items = ({
                     >
                       <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
                     </svg>
-                    <span className="hidden md:block">Share on X / Twitter</span>
+                    <span className="hidden md:block">Share on X</span>
                   </button>
 
                   <div className="relative w-full h-full">
