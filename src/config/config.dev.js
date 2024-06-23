8d2df67cadd3afb69cb9621e018c8d6e3eae02a4
Anthony <10244943+antho31@users.noreply.github.com>
2024-06-23 16:20:08 +0200

    fix: remove math operation on formatted value

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,9 +1,9 @@
 import devEthereumSepolia from "./development/ethereum-sepolia";
-import devBaseSepolia from "./development/base-sepolia";
+// import devBaseSepolia from "./development/base-sepolia";
 
 const config = {
-  11155111: devEthereumSepolia,
-  84532: devBaseSepolia
+  11155111: devEthereumSepolia
+  // 84532: devBaseSepolia
 };
 
 export default config;
