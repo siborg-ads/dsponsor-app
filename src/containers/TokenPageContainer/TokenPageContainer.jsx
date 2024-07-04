7eec63f4b6a77333abcb5157df02073fa6af69a4
Alexandre Trotel <trotelalexandre@proton.me>
2024-07-04 17:19:30 +0200

    fix: image order

--- src/containers/TokenPageContainer/TokenPageContainer.jsx
+++ src/containers/TokenPageContainer/TokenPageContainer.jsx
@@ -1377,6 +1377,7 @@ const TokenPageContainer = () => {
             modalTitle="Ad Space Preview"
             successFullUploadModal={successFullUploadModal}
             isLoadingButton={isLoadingButton}
+            adSubmission={true}
           />
         </div>
       )}
