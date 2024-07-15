a8f047e34eb91160162fd36e2668159f9a5c3af2
Alexandre Trotel <113524174+trotelalexandre@users.noreply.github.com>
2024-07-15 08:36:20 +0200

    add: update offer (#228)
    * add: update offer
    
    * fix: change mint price
    
    * fix: token id
    
    * fix: token id
    
    * fix: mint price
    
    * disable: token mint price

--- src/containers/OfferPageContainer/OfferPageContainer.jsx
+++ src/containers/OfferPageContainer/OfferPageContainer.jsx
@@ -23,6 +23,8 @@ import { ItemsTabs } from "../../components/component";
 import config from "../../config/config";
 import { useSwitchChainContext } from "../../contexts/hooks/useSwitchChainContext";
 import { activated_features } from "../../data/activated_features";
+import UpdateOffer from "../../components/offer-section/updateOffer";
+import ChangeMintPrice from "../../components/offer-section/changeMintPrice";
 
 const OfferPageContainer = () => {
   const router = useRouter();
@@ -57,6 +59,7 @@ const OfferPageContainer = () => {
   const { data: decimalsContract } = useContractRead(tokenContract, "decimals");
   const NATIVECurrency = config[chainId]?.smartContracts?.NATIVE;
   const { setSelectedChain } = useSwitchChainContext();
+  const [canChangeMintPrice, setCanChangeMintPrice] = useState(false);
 
   const { data: bps } = useContractRead(DsponsorAdminContract, "feeBps");
   const maxBps = 10000;
@@ -193,11 +196,18 @@ const OfferPageContainer = () => {
       const fetchAdsOffers = async () => {
         const offer = await fetchOffer(offerId, chainId);
 
-        console.log("offer", offer);
-
         setOfferData(offer);
         if (userAddress && offer?.admins?.includes(userAddress.toLowerCase())) {
           setIsOwner(true);
+        }
+
+        console.log("offer", offer?.nftContract?.owner?.newOwner?.toLowerCase());
+
+        if (
+          userAddress &&
+          userAddress?.toLowerCase() === offer?.nftContract?.owner?.newOwner?.toLowerCase()
+        ) {
+          setCanChangeMintPrice(true);
         }
       };
       setSelectedChain(config[chainId]?.network);
@@ -431,6 +441,11 @@ const OfferPageContainer = () => {
           chainId={chainId}
         />
       </div>
+
+      {isOwner && <ChangeMintPrice offer={offerData} currency={currency} />}
+
+      {isOwner && <UpdateOffer offer={offerData} />}
+
       {!offerData.nftContract.allowList && (
         <div className="container flex flex-col justify-center mb-6">
           <Divider className="my-4" />
