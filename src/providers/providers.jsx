82639452481bee438ff5266ec9fc3f78eb27e558
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-06 16:09:25 +0200

    fix: eoaf

--- src/providers/providers.jsx
+++ src/providers/providers.jsx
@@ -38,8 +38,7 @@ function InnerProviders({ children }) {
         gasless: {
           openzeppelin: {
             relayerUrl:
-              "https://api.defender.openzeppelin.com/actions/60707c07-3e8b-49d6-8912-d529b39d5efa/runs/webhook/59ce7c60-9e0b-4c87-8910-a8b13e6f725e/TEBFAC5SvwbPxoYyYZQR5",
-            useEOAForwarder: true
+              "https://api.defender.openzeppelin.com/actions/60707c07-3e8b-49d6-8912-d529b39d5efa/runs/webhook/59ce7c60-9e0b-4c87-8910-a8b13e6f725e/TEBFAC5SvwbPxoYyYZQR5"
           }
         }
       }}
