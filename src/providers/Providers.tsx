15b1eea64634fdd2f91f73cb01e9f3b5a0b5b4c8
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-12 14:57:39 +0200

    fix: gasless

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -6,6 +6,8 @@ import {
   localWallet,
   metamaskWallet,
   ThirdwebProvider,
+  useAddress,
+  useBalance,
   walletConnect
 } from "@thirdweb-dev/react";
 import ChainProvider from "@/providers/Chain";
@@ -14,6 +16,47 @@ import { useSwitchChainContext } from "@/hooks/useSwitchChainContext";
 import config from "@/config/config";
 import { clientId } from "@/data/services/client";
 import { features } from "@/data/features";
+import { Address } from "thirdweb";
+
+interface GasslessContextValue {
+  balance: bigint | undefined;
+  address: Address | undefined;
+  setBalance: React.Dispatch<React.SetStateAction<bigint | undefined>>;
+  setAddress: React.Dispatch<React.SetStateAction<Address | undefined>>;
+}
+
+const GaslessContext = React.createContext<GasslessContextValue | undefined>(undefined);
+
+function GaslessProvider({ children }: Readonly<{ children: React.ReactNode }>) {
+  const [balance, setBalance] = useState<bigint | undefined>(undefined);
+  const [address, setAddress] = useState<Address | undefined>(undefined);
+
+  const value = { balance, address, setBalance, setAddress };
+
+  return <GaslessContext.Provider value={value}>{children}</GaslessContext.Provider>;
+}
+
+function GaslessCollector({ children }: Readonly<{ children: React.ReactNode }>) {
+  const { setBalance, setAddress } = React.useContext(GaslessContext) as GasslessContextValue;
+
+  const address = useAddress();
+  const { data: balance } = useBalance();
+
+  useEffect(() => {
+    if (balance) {
+      const bigInt = BigInt(balance?.value?.toString());
+      setBalance(bigInt);
+    }
+  }, [balance, setBalance]);
+
+  useEffect(() => {
+    if (address !== undefined) {
+      setAddress(address as Address);
+    }
+  }, [address, setAddress]);
+
+  return <>{children}</>;
+}
 
 function Providers({ children }) {
   return (
@@ -26,6 +69,7 @@ function Providers({ children }) {
 function InnerProviders({ children }: Readonly<{ children: React.ReactNode }>) {
   const [chain, setChain] = useState(Object.values(config)[0]?.network);
   const { selectedChain } = useSwitchChainContext();
+  const { address, balance } = React.useContext(GaslessContext) as GasslessContextValue;
 
   useEffect(() => {
     if (selectedChain) {
@@ -34,36 +78,44 @@ function InnerProviders({ children }: Readonly<{ children: React.ReactNode }>) {
   }, [selectedChain]);
 
   const relayerURL = config?.[chain?.chainId as number]?.features?.openZeppelinDefender?.relayerURL;
+  const balanceThreshold =
+    config?.[chain?.chainId as number]?.features?.crossmint?.config?.priceLimit;
+  const gaslessBalanceCondition = balance && balance < BigInt(balanceThreshold);
 
-  const sdkOptions = features?.canUseGaslessTransactions
-    ? {
-        gasless: {
-          openzeppelin: {
-            relayerUrl: relayerURL
+  const sdkOptions =
+    features?.canUseGaslessTransactions && gaslessBalanceCondition
+      ? {
+          gasless: {
+            openzeppelin: {
+              relayerUrl: relayerURL
+            }
           }
         }
-      }
-    : undefined;
+      : undefined;
 
   return (
-    <ThirdwebProvider
-      {...(sdkOptions && { sdkOptions })}
-      activeChain={chain}
-      clientId={clientId}
-      supportedChains={[Base, Ethereum, BaseSepoliaTestnet, Sepolia, Polygon]}
-      authConfig={{ domain: "dsponsor.com" }}
-      supportedWallets={[
-        metamaskWallet(),
-        coinbaseWallet({ recommended: true }),
-        walletConnect(),
-        localWallet(),
-        embeddedWallet({
-          auth: { options: ["email", "google", "apple", "facebook"] }
-        })
-      ]}
-    >
-      <ChainProvider>{children}</ChainProvider>
-    </ThirdwebProvider>
+    <GaslessProvider>
+      <ThirdwebProvider
+        {...(sdkOptions && { sdkOptions })}
+        activeChain={chain}
+        clientId={clientId}
+        supportedChains={[Base, Ethereum, BaseSepoliaTestnet, Sepolia, Polygon]}
+        authConfig={{ domain: "dsponsor.com" }}
+        supportedWallets={[
+          metamaskWallet(),
+          coinbaseWallet({ recommended: true }),
+          walletConnect(),
+          localWallet(),
+          embeddedWallet({
+            auth: { options: ["email", "google", "apple", "facebook"] }
+          })
+        ]}
+      >
+        <ChainProvider>
+          <GaslessCollector>{children}</GaslessCollector>
+        </ChainProvider>
+      </ThirdwebProvider>
+    </GaslessProvider>
   );
 }
 
